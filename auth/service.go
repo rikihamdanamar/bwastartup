e7ffd834cce9bcc6659c09fd8bd0aa90635f3f49
Muhamad Rifki Hamdan Amar <rifkihamdanamar@gmail.com>
2021-11-06 22:28:19 +0700

    jwt auth, validate token

--- auth/service.go
+++ auth/service.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"errors"
+
 	"github.com/dgrijalva/jwt-go"
 )
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtServices struct {
@@ -28,4 +31,22 @@ func (s *jwtServices) GenerateToken(userID int) (string, error) {
 		return signedToken, err
 	}
 	return signedToken, nil
+}
+
+func (s *jwtServices) ValidateToken(encodedtoken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedtoken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
